cd504aa58f9de8d07f5447119537e804fa5604f8
sukaska <66868058+sukaska@users.noreply.github.com>
2020-08-05 16:49:55 +0300

    upload

---
+++ src/main/java/net/codejava/controller/HomeController.java
@@ -0,0 +1,43 @@
+package net.codejava.controller;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import net.codejava.model.User;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/")
+	public ModelAndView test(HttpServletResponse response) throws IOException {
+		return new ModelAndView("home");
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String viewRegistrationForm(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		List<String> professionList = Arrays.asList("developer", "designer", "archiest");
+		model.addAttribute("professionList", professionList);
+		return "Registration";
+
+	}
+	@RequestMapping(value="/register2",method=RequestMethod.POST)
+	public String doRegistration(@ModelAttribute("user")User user) {
+		System.out.println(user);
+		return "RegistrationSuccess";
+		
+	}
+	
+
+}
